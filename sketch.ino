42856f2d536dfa4400140a2641d19f9567b1bbea
Roboscience GTNK <79186183+GTNK002@users.noreply.github.com>
2022-07-24 16:18:24 +0530

    Update sketch.ino

--- sketch.ino
+++ sketch.ino
@@ -7,7 +7,7 @@
 #include <ESP8266mDNS.h>
 
 const char* ssid = "ESP";
-const char* password = "123456789";
+const char* password = "12345678";
 
 MDNSResponder mdns;
 
@@ -28,7 +28,7 @@ void handleRoot() {
 "<body>" \
 "<h1>Home Automation</h1>" \
 "<h2>"+ switch1name +": "+ switch1 +"</h2>" \
-"<p><a href=\"/Switch1OFF\"><button class=\"button\">OFF</button></a><a href=\"/Switch1ON\"><button class=\"button\">ON</button></a></p>" \
+"<p><a href=\"/Switch1ON\"><button class=\"button\">ON</button></a><a href=\"/Switch1OFF\"><button class=\"button\">OFF</button></a></p>" \
 "<h2>Fan: "+ Fan +"</h2>" \
 "<p><a href=\"/FanOFF\"><button class=\"button\">OFF</button></a><a href=\"/FanSpeed1\"><button class=\"button\">Speed1</button></a><a href=\"/FanSpeed2\"><button class=\"button\">Speed2</button></a><a href=\"/FanSpeed3\"><button class=\"button\">Speed3</button></a><a href=\"/FanSpeed4\"><button class=\"button\">Speed4</button></a></p>" \
 "</body>" \
@@ -50,59 +50,64 @@ void switch1OFF(){
 
 void FanOFF(){
   Fan = "OFF";
+  digitalWrite(D5, HIGH);
   digitalWrite(D6, HIGH);
   digitalWrite(D7, HIGH);
-  digitalWrite(D8, HIGH);
   handleRoot();
 }
 
 void FanSpeed1(){
   Fan = "Speed1";
+  digitalWrite(D5, HIGH);
+  digitalWrite(D6, HIGH);
   digitalWrite(D7, HIGH);
-  digitalWrite(D8, HIGH);
   delay(500);
-  digitalWrite(D6, LOW);
+  digitalWrite(D5, LOW);
   handleRoot();
 }
 
 void FanSpeed2(){
   Fan = "Speed2";
+  digitalWrite(D5, HIGH);
   digitalWrite(D6, HIGH);
-  digitalWrite(D8, HIGH);
+  digitalWrite(D7, HIGH);
   delay(500);
-  digitalWrite(D7, LOW);
+  digitalWrite(D6, LOW);
+  handleRoot();
 }
 
 void FanSpeed3(){
   Fan = "Speed3";
-  digitalWrite(D8, HIGH);
+  digitalWrite(D5, HIGH);
+  digitalWrite(D6, HIGH);
   digitalWrite(D7, HIGH);
   delay(500);
+  digitalWrite(D5, LOW);
   digitalWrite(D6, LOW);
-  digitalWrite(D7, LOW);
   handleRoot();
 }
 
 void FanSpeed4(){
   Fan = "Speed4";
+  digitalWrite(D5, HIGH);
   digitalWrite(D6, HIGH);
   digitalWrite(D7, HIGH);
   delay(500);
-  digitalWrite(D8, LOW);
+  digitalWrite(D7, LOW);
   handleRoot();
 }
 
 void setup(void){
   pinMode(D1, OUTPUT);
   pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
   pinMode(D6, OUTPUT);
   pinMode(D7, OUTPUT);
-  pinMode(D8, OUTPUT);
-  digitalWrite(D4, HIGH);
   digitalWrite(D1, HIGH);
+  digitalWrite(D4, HIGH);
+  digitalWrite(D5, HIGH);
   digitalWrite(D6, HIGH);
   digitalWrite(D7, HIGH);
-  digitalWrite(D8, HIGH);
   
   Serial.begin(115200);
   
@@ -126,12 +131,12 @@ void setup(void){
  
   server.on("/", handleRoot);   
   server.on("/Switch1ON", switch1ON);  
-  server.on("/Switch1OFF", switch1OFF);  
-  server.on("/FanOFF", FanOFF);  
-  server.on("/FanSpeed1", FanSpeed1);  
-  server.on("/FanSpeed2", FanSpeed2); 
-  server.on("/FanSpeed3", FanSpeed3); 
-  server.on("/FanSpeed4", FanSpeed4);     
+  server.on("/Switch1OFF", switch1OFF);      
+  server.on("/FanOFF", FanOFF); 
+  server.on("/FanSpeed1", FanSpeed1);   
+  server.on("/FanSpeed2", FanSpeed2);
+  server.on("/FanSpeed3", FanSpeed3);
+  server.on("/FanSpeed4", FanSpeed4);  
 
   server.begin();                 
   Serial.println("HTTP server started");
